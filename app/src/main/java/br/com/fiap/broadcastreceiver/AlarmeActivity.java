9530943b42c0371d8d15f99557ef5b08c38f8219
Guilherme <guilherme@ztrade.com>
2016-11-21 19:22:56 -0200

    botão configurar e novo reciver para alarme

--- app/src/main/java/br/com/fiap/broadcastreceiver/AlarmeActivity.java
+++ app/src/main/java/br/com/fiap/broadcastreceiver/AlarmeActivity.java
@@ -2,12 +2,23 @@ package br.com.fiap.broadcastreceiver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AlarmeActivity extends AppCompatActivity {
 
+    EditText edtTempo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarme);
+        edtTempo = (EditText)findViewById(R.id.edtTempo);
+    }
+
+    public void configurar(View v){
+        String tempo = edtTempo.getText().toString();
+        if(!tempo.equals("")){
+            int valTempo = Integer.parseInt(tempo);
+        }
     }
 }
--- app/src/main/java/br/com/fiap/broadcastreceiver/reciver/AlarmeReciver.java
+++ app/src/main/java/br/com/fiap/broadcastreceiver/reciver/AlarmeReciver.java
@@ -3,16 +3,15 @@ package br.com.fiap.broadcastreceiver.reciver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 /**
  * Created by guilherme on 21/11/16.
  */
 
-public class MySecondReciver extends BroadcastReceiver {
+public class AlarmeReciver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        Toast.makeText(context,"Nvo Reciver 2",Toast.LENGTH_LONG).show();
+        
     }
 }
