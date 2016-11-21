139cdc274c330badb95aea7ab4275a04574f82e5
Guilherme <guilherme@ztrade.com>
2016-11-21 20:25:46 -0200

    criada notificação

--- app/src/main/java/br/com/fiap/broadcastreceiver/receiver/AlarmeReceiver.java
+++ app/src/main/java/br/com/fiap/broadcastreceiver/receiver/AlarmeReceiver.java
@@ -1,9 +1,14 @@
 package br.com.fiap.broadcastreceiver.receiver;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
 import android.widget.Toast;
+
+import br.com.fiap.broadcastreceiver.AlarmeActivity;
+import br.com.fiap.broadcastreceiver.R;
 
 /**
  * Created by guilherme on 21/11/16.
@@ -14,5 +19,23 @@ public class AlarmeReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context,"Alarme disparado",Toast.LENGTH_LONG).show();
+        enviarNotificacao(context);
+    }
+
+    private void enviarNotificacao(Context context){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        builder.setContentIntent(
+                PendingIntent.getActivity(context,0,
+                        new Intent(context, AlarmeActivity.class),
+                        PendingIntent.FLAG_UPDATE_CURRENT));
+        builder.setContentTitle("Alarme disparado");
+        builder.setContentText("Seu alarme foi disparado com sucesso");
+        builder.setTicker("Nova Mensagem");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setAutoCancel(true);
+        
+
+
     }
 }
